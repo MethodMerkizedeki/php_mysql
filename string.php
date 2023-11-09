72457360e4858efe4312f3c83c01d53dba43df7a
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-09 13:22:11 +0300

    php string

---
+++ string.php
@@ -0,0 +1,18 @@
+<?php
+
+//string concatanation
+$string_one = 'my email is ';
+$string_two = 'mmerkizedeki@gmail.com';
+
+//echo $string_one . $string_two;
+
+//string functions
+
+$name = 'mark';
+
+//echo strlen($name);
+//echo strtoupper($name);
+//echo strtolower(name);
+echo str_replace('m', 'r', $name);
+
+?>
