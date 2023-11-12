da6353d319597b24f311cb1e3ffd3c8bcfaeca62
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-12 11:09:23 +0300

    comparisons and booleans

---
+++ comparison.php
@@ -0,0 +1,26 @@
+<?php
+//comparisons booleans (true or false)
+
+//numbers
+//echo 5 < 10;
+echo 5 > 10;
+echo 5 == 10;
+//echo 5 <= 10;
+echo 5 != 10;
+
+//strings
+
+echo 'mark' > 'method';
+//echo 'mark' == 'method';
+//echo 'mark' != 'method';
+echo 'mark' < 'method';
+echo 'Mark' < 'mark';
+
+//loose vs strict equal comparison
+echo 5 == '5';
+echo 5 === '5';
+//echo 5 === 5;
+
+//echo true == '1';
+echo false == '';
+?>
