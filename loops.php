3a6161d3e0b78c79d26e3844868c37a19b74697e
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-12 08:40:37 +0300

    loops in php

---
+++ loops.php
@@ -0,0 +1,44 @@
+<?php
+
+$names = ['method', 'bukuru', 'merkizedeki'];
+
+//for($i = 0; $i < count($names); $i++){
+    //echo $names[$i] . '<br>';
+//}
+ 
+foreach($names as $name){
+    echo $name . '<br>';
+}
+
+$products = [
+    ['name' => 'maize', 'price' => 789],
+    ['name' => 'rice', 'price' => 345],
+    ['name' => 'banana', 'price' => 123],
+    ['name' => 'mango', 'price' => 100]
+];
+
+foreach($products as $product){
+    echo $product['name'] . ' - ' . $product['price'];
+    echo '<br>';
+}
+
+$i = 0;
+while($i < count($products)){
+    echo $products[$i]['name'];
+    echo '<br>';
+    $i++;
+}
+?>
+
+<html>
+<body>
+    <h1>Products</h1>
+    <ul>
+        <?php foreach($products as $product){?>
+
+           <h3><?php echo $product['name']; ?></h3>
+           <p>Tsh <?php echo $product['price']; ?></p>
+        <?php } ?>
+    </ul>
+</body>
+</html>
