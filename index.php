baa72d07d87ff706bca9732d7ec6f48d4ddd0845
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-15 18:14:33 +0300

    include and require in php

--- index.php
+++ index.php
@@ -1,8 +1,19 @@
+<?php
+
+//include functions
+
+include('method.php');
+echo 'end of php';
+
+?>
+
 <html>
     <body>
     <h1>
         <?php
-         echo 'software development';
+         //echo 'software development';
+
+         include('contents.php');
         ?>
     </h1>
     </body>
---
+++ method.php
@@ -0,0 +1,6 @@
+<?php
+
+$names = ['method', 'bukuru', 'merkizedeki'];
+echo $names[1] . '<br>';
+
+?>
