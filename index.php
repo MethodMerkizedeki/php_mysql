be5d89e926465ead13a892fcb322832ec4bc86c5
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-20 17:22:25 +0300

    connecting to a database

--- index.php
+++ index.php
@@ -5,6 +5,14 @@
 //include('method.php');
 //echo 'end of php';
 
+//connet to database
+$conn = mysqli_connect('localhost', 'method', '201611038', 'mark_pizza');
+
+//check the connection
+if(!$conn){
+  echo 'connection erroe: ' . mysqli_connect_error();
+}
+
 ?>
 
 <html>
