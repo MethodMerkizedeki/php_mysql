f515c422a65511b8606f056ab6b2cd5e8bf2d263
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-20 17:41:31 +0300

    getting data from a database

--- index.php
+++ index.php
@@ -13,6 +13,22 @@ if(!$conn){
   echo 'connection erroe: ' . mysqli_connect_error();
 }
 
+//write query for all pizzas
+$sql = 'SELECT title, ingredients, id FROM pizzas';
+
+//make query or get results
+$result = mysqli_query($conn, $sql);
+
+//fetch the resulting row as an array
+$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+//free the result form memory
+mysqli_free_result($result);
+
+//close connection
+mysqli_close($conn);
+
+print_r($pizzas); 
 ?>
 
 <html>
