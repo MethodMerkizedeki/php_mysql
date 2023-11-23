617e7c3ce2d4f9be48bb02e9627b924188b4d5db
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-23 19:53:10 +0300

    getting a single record

--- templates/config/db_connect.php
+++ templates/config/db_connect.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect('localhost', 'method', '201611038', 'mark_pizza');
 
 //check the connection
 if(!$conn){
-  echo 'connection erroe: ' . mysqli_connect_error();
+  echo 'connection error: ' . mysqli_connect_error();
 }
 
 ?>
