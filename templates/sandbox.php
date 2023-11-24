660e16620c9f83c10b99d47fcbd72cf46b379850
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-24 19:37:52 +0300

    ternary operator

---
+++ templates/sandbox.php
@@ -0,0 +1,18 @@
+<?php
+
+//ternary operator
+
+$score = 30;
+// if($score >= 50 ){
+//     echo 'high score';
+// } else {
+//     echo 'low score';
+// }
+
+//echo $score >= 50 ? 'high score' : 'low score';
+
+?>
+
+<html>
+    <p><?php echo $score >= 50 ? 'high score' : 'low score'; ?></p>
+</html>
