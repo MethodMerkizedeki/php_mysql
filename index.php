3a0e6c97b2086d43b8375d137ea16ee145c44a57
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-11-24 19:19:44 +0300

    finishing touches

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ mysqli_close($conn);
            <?php foreach($pizzas as $pizza): ?>
               <div class="col s6 md3">
                  <div class="card z-depth-0">
+                  <img src="img/pizza.png" alt="" class ="pizza">
                    <div class="card-content center">
                       <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                       <ul><?php foreach(explode(',', $pizza['ingredients']) as $ing): ?>
