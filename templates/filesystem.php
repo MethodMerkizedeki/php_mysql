9d7f8dfe9bb85adf725c71d92d7e2b147d249e09
MethodMerkizedeki <mmerkizedeki@gmail.com>
2023-12-01 18:02:25 +0300

    file system in php

---
+++ templates/filesystem.php
@@ -0,0 +1,27 @@
+<?php
+
+$file = 'readme.txt';
+
+if(file_exists($file)){
+    //read file
+    echo readfile($file) . '<br>';
+
+    //copy file
+    copy($file, 'quotes.txt') . '<br>';
+
+    //absolute path
+    echo realpath($file) . '<br>';
+
+    //file size
+    echo filesize($file) . '<br>';
+
+    //rename the file
+    echo rename($file, 'test.txt');
+} else {
+    echo 'file does not exist';
+}
+
+//make directory
+mkdir('quotes');
+
+?>
